8ed94e661bd803dae88ec576c2f6e68e74a1f5b5
causality-3 <abinbabu003@gmail.com>
2025-05-19 22:21:33 +0530

    Ip fix done

--- utils/helper-functions.go
+++ utils/helper-functions.go
@@ -175,8 +175,6 @@ func MnitorSystem() {
 func getVisitor(ip string) *rate.Limiter {
 	mu.Lock()
 	defer mu.Unlock()
-
-	fmt.Println("Visitor IP", ip)
 	v, exists := visitors[ip]
 	if !exists {
 		limiter := rate.NewLimiter(5, 10) // 5 requests per second, burst of 10
@@ -201,15 +199,20 @@ func CeanupVisitors() {
 	}
 }
 
-func RateLimiterMiddleware(c *fiber.Ctx) error {
-	// if rateLimiting {
-	// return c.Next()
-	fmt.Println("X-Real-IP:", c.Get("X-Real-IP"))
-	fmt.Println("X-Forwarded-For:", c.Get("X-Forwarded-For"))
-	fmt.Println("RemoteAddr:", c.Context().RemoteAddr().String())
-	fmt.Println("Fiber IP:", c.IP())
+func getClientIP(c *fiber.Ctx) string {
+	if xff := c.Get("X-Forwarded-For"); xff != "" {
+		parts := strings.Split(xff, ",")
+		return strings.TrimSpace(parts[0])
+	}
+	if xrip := c.Get("X-Real-IP"); xrip != "" {
+		return xrip
+	}
+	return c.IP()
+}
 
-	ip := c.IP()
+func RateLimiterMiddleware(c *fiber.Ctx) error {
+
+	ip := getClientIP(c)
 	limiter := getVisitor(ip)
 
 	if !limiter.Allow() {
